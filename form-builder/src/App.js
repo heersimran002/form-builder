4bfc788d20c9b0208bd427c1faecbd68003105ab
heersimran002 <heersimran002@gmail.com>
2022-12-19 15:06:19 -0600

    INITIAL COMMIT

---
+++ form-builder/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="text-3xl font-bold underline bg-red-400">
+          Hello world!
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
